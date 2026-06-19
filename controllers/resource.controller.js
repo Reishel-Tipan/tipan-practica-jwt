748f646de8977d95be4a0115ae6a000f429f93c1
Reishel-Tipan <reisheltipan@gmail.com>
2026-06-19 07:09:18 -0500

    feat: simular endpoints independientes para service-alpha y service-beta

--- controllers/resource.controller.js
+++ controllers/resource.controller.js
@@ -1,19 +1,25 @@
 export class ResourceController {
-    /**
-     * Simula un recurso privado del Microservicio Alpha.
-     */
     static getAlphaPrivateData(req, res) {
-        // TODO: Implementar respuesta para Microservicio Alpha.
-        // 1. Responder con un mensaje que indique acceso exitoso al Servicio Alpha.
-        // 2. Incluir datos del usuario autenticado (req.user) en la respuesta.
+        return res.status(200).json({
+            service: 'service-alpha',
+            message: 'Acceso autorizado al Microservicio Alpha',
+            authenticated_user: {
+                id: req.user.sub,
+                name: req.user.name,
+            },
+            timestamp: new Date().toISOString(),
+        });
     }
 
-    /**
-     * Simula un recurso privado del Microservicio Beta.
-     */
     static getBetaPrivateData(req, res) {
-        // TODO: Implementar respuesta para Microservicio Beta.
-        // 1. Responder con un mensaje que indique acceso exitoso al Servicio Beta.
-        // 2. Incluir datos del usuario autenticado (req.user) en la respuesta.
+        return res.status(200).json({
+            service: 'service-beta',
+            message: 'Acceso autorizado al Microservicio Beta',
+            authenticated_user: {
+                id: req.user.sub,
+                name: req.user.name,
+            },
+            timestamp: new Date().toISOString(),
+        });
     }
 }
